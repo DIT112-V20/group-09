37c1ba4ae659fe6478d8ac5900a236c5f49243cd
BhavyaShukla <bhavyashukla2002@gmail.com>
2020-05-11 23:15:54 +0200

    added android library

---
+++ extras/mobile_app/mobile_remote_kit/src/main/java/com/example/mobile_remote_kit/test.kt
@@ -0,0 +1,12 @@
+package com.example.library
+
+import android.util.Log
+
+class test {
+
+    private val TAG: String  = "LIB_TEST"
+
+    fun testing (message : String) {
+        Log.d(TAG, message)
+    }
+}
