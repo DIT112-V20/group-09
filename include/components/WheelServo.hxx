276f1a07729607f8e1a96f397edac637e26adbc4
Ruthger Dijt <ruthgerdijt@gmail.com>
2020-05-31 20:32:08 +0200

    [WheelServo] Initial implementation

---
+++ include/components/WheelServo.hxx
@@ -0,0 +1,80 @@
+/*
+ *  WheelServo.hxx
+ *  Copyright 2020 ItJustWorksTM
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#ifndef SMARTCAR_EMUL_WHEELSERVO_HXX
+#define SMARTCAR_EMUL_WHEELSERVO_HXX
+
+#include <Urho3D/Physics/PhysicsEvents.h>
+#include <Urho3D/Physics/RaycastVehicle.h>
+#include "Servo.hxx"
+
+// GCC 9 workaround
+enum class RayMembers { SetSteeringValue, SetEngineForce };
+template <void (Urho3D::RaycastVehicle::*mem_fn)(int, float)> auto to_enum();
+template <> constexpr auto to_enum<&Urho3D::RaycastVehicle::SetSteeringValue>() { return RayMembers::SetSteeringValue; }
+template <> constexpr auto to_enum<&Urho3D::RaycastVehicle::SetEngineForce>() { return RayMembers::SetEngineForce; }
+
+template <void (Urho3D::RaycastVehicle::*mptr)(int, float)> class WheelServo : public Servo {
+    URHO3D_OBJECT(WheelServo, Servo);
+    static constexpr auto err_msg = "Attempted to create component ServoMotor with an invalid configuration";
+
+    Urho3D::RaycastVehicle* vehicle;
+    std::vector<unsigned int> wheels;
+    float max_speed;
+
+  public:
+    explicit WheelServo(BoardData& bd, Urho3D::Node* node, const rapidjson::Value& pin)
+        : Servo{bd, node, pin}, vehicle{node->GetComponent<Urho3D::RaycastVehicle>(true)}, wheels{} {
+        if (!pin.HasMember("wheels"))
+            throw std::runtime_error{err_msg};
+        if (!pin.HasMember("max_speed"))
+            throw std::runtime_error{err_msg};
+        pwm_pin->store(90);
+        max_speed = pin["max_speed"].GetFloat();
+        if (vehicle)
+            for (const auto& x : pin["wheels"].GetArray()) {
+                auto val = x.GetUint();
+                if (val > vehicle->GetNumWheels())
+                    throw std::runtime_error{err_msg};
+                wheels.push_back(val);
+            }
+    }
+
+    void Update(float timeStep) override {
+        if constexpr (to_enum<mptr>() == RayMembers::SetSteeringValue) {
+            for (const auto& no : wheels)
+                (vehicle->*mptr)(no, deg2rad(math_map(+pwm_pin->load(), 0, 180, movement_mode->angle_min, movement_mode->angle_max)));
+        } else if constexpr (to_enum<mptr>() == RayMembers::SetEngineForce) {
+            for (const auto& no : wheels)
+                (vehicle->*mptr)(no, math_map(static_cast<float>(+pwm_pin->load()), 0.f, 180.f, -1.f, +1.f) * max_speed);
+        }
+    }
+};
+
+extern template class WheelServo<&Urho3D::RaycastVehicle::SetSteeringValue>;
+extern template class WheelServo<&Urho3D::RaycastVehicle::SetEngineForce>;
+
+struct SteeringServo : WheelServo<&Urho3D::RaycastVehicle::SetSteeringValue> {
+    URHO3D_OBJECT(SteeringServo, WheelServo<&Urho3D::RaycastVehicle::SetSteeringValue>);
+    using WheelServo::WheelServo;
+};
+struct ServoMotor : WheelServo<&Urho3D::RaycastVehicle::SetEngineForce> {
+    URHO3D_OBJECT(ServoMotor, WheelServo<&Urho3D::RaycastVehicle::SetEngineForce>);
+    using WheelServo::WheelServo;
+};
+
+#endif // SMARTCAR_EMUL_WHEELSERVO_HXX
---
+++ src/components/Servo.cxx
@@ -0,0 +1,52 @@
+/*
+ *  Servo.cxx
+ *  Copyright 2020 ItJustWorksTM
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#include "components/Servo.hxx"
+
+constexpr auto err_msg = "Attempted to create component Servo with an invalid configuration";
+
+Servo::Servo(BoardData& bd, Urho3D::Node* node, const rapidjson::Value& pin) : Urho3D::LogicComponent{node->GetContext()}, node{node} {
+    if (!pin.HasMember("pin"))
+        throw std::runtime_error{err_msg};
+
+    auto pin_no = pin["pin"].GetUint();
+
+    if (pin_no > bd.analog_pin_values.size())
+        throw std::runtime_error{err_msg};
+
+    pwm_pin = &bd.pwm_values[pin_no];
+
+    if (pin.HasMember("rotation")) {
+        if (pin["rotation"].IsObject()) {
+            auto tmp = pin["rotation"].GetObject();
+            movement_mode.emplace() = {tmp["angle_min"].GetFloat(), tmp["angle_max"].GetFloat()};
+        }
+    }
+}
+
+void Servo::Update(float timeStep) {
+    auto rotate = node_->GetRotation();
+    auto val = +pwm_pin->load();
+    Urho3D::Vector3 rotation{};
+    if (movement_mode)
+        rotation.y_ = deg2rad(math_map(val, 0, 180, movement_mode->angle_min, movement_mode->angle_max));
+    else {
+        rotation = rotate.EulerAngles();
+        rotation.y_ += deg2rad(static_cast<decltype(rotation.y_)>(val));
+    }
+    node_->SetRotation({rotation.x_, rotation.y_, rotation.z_});
+}
